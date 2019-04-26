c7d9d3fd58b58e8814673148efbd014f6f9b2c98
Jeferson <mx.jeferson.12@gmail.com>
2019-04-26 12:47:36 -0300

    1.8: details and minify

--- pages/footer.php
+++ pages/footer.php
@@ -1,8 +1,8 @@
 <footer class="w3-display-container w3-black w3-padding">
   <nav class="w3-bar menu">
-    <div class="w3-bar-item "><a href="#" target="_blank"><img src="../img/facebook.png"></a></div>
+    <div class="w3-bar-item "><a href="https://facebook.com/Mx.Jeff.10" target="_blank"><img src="../img/facebook.png"></a></div>
     <div class="w3-bar-item "><a href="https://github.com/DZ0Jeff" target="_blank"><img src="../img/github3.png" ></a></div>
-    <div class="w3-bar-item "><a href="#" target="_blank"><img src="../img/linkedin.png"></a></div>
+    <div class="w3-bar-item "><a href="https://www.linkedin.com/in/jeferson-morais-590835145" target="_blank"><img src="../img/linkedin.png"></a></div>
   </nav>
   <div class="w3-display-middle w3-padding">@DZjeff</div>
 </footer>
